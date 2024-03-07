6ff0e61b83855c7205c4ed34eccbf6fa7c2834be
philip-barcena <124049418+philip-barcena@users.noreply.github.com>
2024-03-07 16:24:59 +0000

    GameDifficulty
    Difficulty increase based on how long player is alive

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -8,24 +8,37 @@ public class Spawner : MonoBehaviour
     [SerializeField] private GameObject[] obstaclePrefabs;
     [SerializeField] private Transform obstacleParent;
     public float obstacleSpawnTimer = 2f;
+
+    [Range(0, 1)] public float obstacleSpawnTimeFactor = 0.1f;
     public float obstacleVelocity = 1f;
 
+    [Range(0, 1)] public float obstacleVelocityFactor = 0.2f;
+
+    private float _obstacleSpawnTimer;
+    private float _obstacleVelocity;
+
+    private float timeAlive;
     private float timeUntilObsSpawn;
 
     private void Start() {
         GameManager.Instance.onGameOver.AddListener(ClearObstacles);
-
+        GameManager.Instance.onPlay.AddListener(ResetTimers);
+        
     }
 
     private void Update() {
         if (GameManager.Instance.isPlaying) {
+            timeAlive += Time.deltaTime;
+
+        CalculateFactors();
+
             SpawnLoop();
         }
     }
     private void SpawnLoop() {
         timeUntilObsSpawn += Time.deltaTime;
 
-        if(timeUntilObsSpawn >= obstacleSpawnTimer){
+        if(timeUntilObsSpawn >= _obstacleSpawnTimer){
             Spawn();
             timeUntilObsSpawn = 0f;
         }
@@ -37,13 +50,24 @@ public class Spawner : MonoBehaviour
         }
     }
 
+    private void CalculateFactors() {
+        _obstacleSpawnTimer = obstacleSpawnTimer / Mathf.Pow(timeAlive, obstacleSpawnTimeFactor);
+        _obstacleVelocity = obstacleVelocity * Mathf.Pow(timeAlive, obstacleVelocityFactor);
+    }
+
+    private void ResetTimers () {
+        timeAlive = 1f;
+        _obstacleSpawnTimer = obstacleSpawnTimer;
+        _obstacleVelocity = obstacleVelocity;
+    }
+
     private void Spawn() {
         GameObject obstacleSpawn = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
         GameObject spawnedObstacle = Instantiate(obstacleSpawn, transform.position, Quaternion.identity);
         spawnedObstacle.transform.parent = obstacleParent;
 
         Rigidbody2D obstacleRB = spawnedObstacle.GetComponent<Rigidbody2D>();
-        obstacleRB.velocity = Vector2.left * obstacleVelocity;
+        obstacleRB.velocity = Vector2.left * _obstacleVelocity;
     }
 
 
